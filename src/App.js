236d918ebcc9a56d6d04e1c1ebd7392e154bbce5
creammie888 <cream280745@gmail.com>
2025-04-29 13:15:43 +0700

    3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Result from './pages/result';
 import Index1 from './pages/index1';
 import Index2 from './pages/index2';
 import Pick from './pages/pick';
+import Type from './pages/selectType';
+import TarotType from './pages/selectTypeofTarot';
+import Sticks from './pages/fortuneSticks';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route path="/wish" element={<Index2 />} />
           <Route path="/result" element={<Result />} />
           <Route path="/pick" element={<Pick />} />
+          <Route path="/selectType" element={<Type />} />
+          <Route path="/selectTypeofTarot" element={<TarotType />} />
+          <Route path="/fortuneSticks" element={<Sticks />} />
         </Routes>
       </main>
     </Router>
