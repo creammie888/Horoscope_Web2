7f06f7c78b7f875098076eb5e9206253facca1c0
opolka <opnrxsh@gmail.com>
2025-05-01 06:07:18 +0700

    kuy

--- src/pages/fortuneSticks_result.jsx
+++ src/pages/fortuneSticks_result.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import '../css/fortuneSticksResult.css';
-import { useState, useEffect } from 'react';
+
 
 
 
